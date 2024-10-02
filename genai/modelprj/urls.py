042038b53480675004b4ddb21ee32b15cb43a236
keerthana2-005 <153196340+keerthana2-005@users.noreply.github.com>
2024-10-02 06:44:09 +0530

    Add files via upload

---
+++ genai/modelprj/urls.py
@@ -0,0 +1,38 @@
+"""
+URL configuration for modelprj project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from modelapp import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth.views import LogoutView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.welcome,name="welcome"),
+    path('login/',views.login,name="login"),
+    path('profile/',views.profile,name="profile"),
+    path('contact/',views.contact,name="contact"),
+    path('signup/',views.signup,name="signup"),
+    # path('index/',views.index,name="index"),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('base/',views.base,name="base"),
+    path('home/',views.home,name="home")
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
